7b9f7684f67349b3924a4dd1498e180b7a3bcc4b
network-Error <iamimew@gmail.com>
2023-05-09 00:13:53 +0300

    feate: add task solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const pathToNewFiles = path.join(__dirname, 'files-copy');
+const pathToOldFiles = path.join(__dirname, 'files');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+    if (err) throw err;
+});
+
+fs.readdir(pathToOldFiles, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+        const pathFrom = path.join(pathToOldFiles, file);
+        const pathTo = path.join(pathToNewFiles, file);
+        fs.copyFile(pathFrom, pathTo, (err) => {
+            if (err) throw err;
+        })
+    })
+    console.log('Копирование файлов завершено!');
+})
